e379cc6ab4750bf2e50f6435a0668f886ba77e34
haleek <100880849+haleek@users.noreply.github.com>
2022-03-03 15:39:57 -0800

    Update settings.py

--- tomato/settings.py
+++ tomato/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '-0b)p%uu0w^c4i7lq%kpr4^70v5lhyq7)=q=z1+1u@&8by0fe3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tomatokiza.herokuapp.com','127.0.0.1']
 
 
 # Application definition

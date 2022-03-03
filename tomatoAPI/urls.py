5e7241e66f891a6c5a80c410945740a7f4e84170
haleek <100880849+haleek@users.noreply.github.com>
2022-03-03 15:03:45 -0800

    Update urls.py

--- tomatoAPI/urls.py
+++ tomatoAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from tomatoAPI import views
+from django.http import HttpResponse
 
 urlpatterns = [
-    
+    path('', lambda request: HttpResponse('Hello World!'), name='hello_world'),
     path('predict/', views.predict.as_view())
 ]
